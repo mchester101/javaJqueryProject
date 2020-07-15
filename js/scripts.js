c4bdac60df4039cd6a71510f15425a694662c8b1
mchester101 <mchester101@gmail.com>
2020-07-15 13:55:28 -0700

    added div class questions to all quiz questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
+
+    // var totalQuestions = $('.questions'),size();
+    // var currentQueston = 0;
+    // $questions = $('.questions');
+    // $questions.hide();
+    // $($questions.get(currentQuestions)).fadeIn();
+    // $('.next').click(function () {
+    //   $($questions.get(currentQuestions +1;
+    //     if (currentQuestions == totalQuestions {
+    //       var result sum_values()
+    //       alert(result);
+    //     } else {
+    //       $($questions.get(currentQuestion)).fadeIn();
+    //     }
+    //     });
+    // });  
 $(document).ready(function() {
-  $("form#quiz").submit(function() {
-  event.preventDefault();
-  const answer1 = parseInt($("input:radio[name=question1]:checked").val());
-  console.log(answer1);
-  const answer2 = parseInt($("input:radio[name=question2]:checked").val());
-  console.log(answer2);
-  const answer3 = parseInt($("input:radio[name=question3]:checked").val());
-  console.log(answer3);
-  const total = answer1 + answer2 + answer3;
-  let result;
-  if (total <= 5) {
+  $("form#quiz").submit(function() {    
+    event.preventDefault();
+    const answer1 = parseInt($("input:radio[name=question1]:checked").val());
+    console.log(answer1);
+    const answer2 = parseInt($("input:radio[name=question2]:checked").val());
+    console.log(answer2);
+    const answer3 = parseInt($("input:radio[name=question3]:checked").val());
+    console.log(answer3);
+    const total = answer1 + answer2 + answer3;
+    let result;
+    if (total <= 5) {
     result = "You should learn Python!"
   } else if (total >=6 && total <= 15) {
     result = "Your time would be best spent learning Swift."
   } else {
     result = "You would be most happy learning C#."
   }
-
-  console.log(total);
-  console.log(result);
 
   $("#output").text(result);
 
