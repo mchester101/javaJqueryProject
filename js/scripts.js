5e982a38f610b2b7c8b7eb9c92310e7bb485f952
mchester101 <mchester101@gmail.com>
2020-07-15 11:12:15 -0700

    copied code from character practice quiz, made adjustments to html and java to fit needs of this project, added image to html, added new html page, attpemting to make landing page link to quiz

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function() {
+  event.preventDefault();
+  const answer1 = parseInt($("input:radio[name=question1]:checked").val());
+  console.log(answer1);
+  const answer2 = parseInt($("input:radio[name=question2]:checked").val());
+  console.log(answer2);
+  const answer3 = parseInt($("input:radio[name=question3]:checked").val());
+  console.log(answer3);
+  const total = answer1 + answer2 + answer3;
+  let result;
+  if (total <= 5) {
+    result = "You should learn Python!"
+  } else if (total >=6 && total <= 15) {
+    result = "Your time would be best spent learning Swift."
+  } else {
+    result = "You would be most happy learning C#."
+  }
+
+  console.log(total);
+  console.log(result);
+
+  $("#output").text(result);
+
+  });
+});
